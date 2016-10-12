5ae015c5628540fa626101f5757c0a1620e4f3d0
Mohammad Awwad <msa492@gmail.com>
2016-10-12 11:32:17 +0300

    Swift 2.3

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -20,6 +20,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func experiment() {
+        let nextController = UIImagePickerController()
+        self.presentViewController(nextController, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func alertF(sender: AnyObject) {
+        
+        let controller = UIAlertController()
+        controller.title = "Test alert"
+        controller.message = "This is a test"
+        
+        // Dismiss the view controller after the user taps “ok”
+        let okAction = UIAlertAction (title:"ok", style: UIAlertActionStyle.Default) {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        controller.addAction(okAction)
+        self.presentViewController(controller, animated: true, completion:nil)
+        
+    }
 
+    @IBAction func activeV(sender: AnyObject) { //share model
+        
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
 }
 
